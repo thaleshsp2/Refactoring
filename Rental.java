42b6d04d2464ec8ea9efd01b0e929a23d5599560
Thales <thales.henrique@engenharia.ufjf.br>
2022-02-04 20:14:30 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -17,24 +17,34 @@ public class Rental {
     }
 
     public double getCharge() {
-      //determine amounts for each line
-      double thisAmount = 0;
+        //determine amounts for each line
+        double thisAmount = 0;
         switch (getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (getDaysRented() > 2)
-                 thisAmount += (getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (getDaysRented() > 3)
-                 thisAmount += (getDaysRented() - 3) * 1.5;
-               break;
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (getDaysRented() > 2)
+                    thisAmount += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (getDaysRented() > 3)
+                    thisAmount += (getDaysRented() - 3) * 1.5;
+                break;
         }
-      return thisAmount;
+        return thisAmount;
+    }
+
+    public int getFrequentRenterPoints() {
+        // add frequent renter points
+        int frequentRenterPoints = 1;
+
+        // add bonus for a two day new release rental
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+            getDaysRented() > 1) frequentRenterPoints++;
+        return frequentRenterPoints;
     }
 
     public static void main(String[] args) {}
