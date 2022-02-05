8c77a13f18d345197b9de9fe47ac252e82c7eac6
Thales <thales.henrique@engenharia.ufjf.br>
2022-02-04 23:37:47 -0300

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -2,23 +2,42 @@ import java.util.Enumeration;
 
 public class HtmlStatement extends Statement {
 
-   public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
-      "</EM></H1><P>\n";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for each rental
-         result += each.getMovie().getTitle()+ ": " +
-         String.valueOf(each.getCharge()) + "<BR>\n";
-      }
-      //add footer lines
-      result += "<P>You owe <EM>" +
-      String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-      result += "On this rental you earned <EM>" +
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      "</EM> frequent renter points<P>";
-      return result;
-   }
-   public static void main(String[] args) {}
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = getHeadValue(aCustomer);
+
+        while (rentals.hasMoreElements()) {
+
+            Rental each = (Rental) rentals.nextElement();
+            //show figures for each rental
+            result += getRentalValue(each);
+        }
+        //add footer lines
+        result += getFooterValue(aCustomer);
+        return result;
+    }
+
+
+    public String getRentalValue(Rental each) {
+        return each.getMovie().getTitle() + ": " +
+            String.valueOf(each.getCharge()) + "<BR>\n";
+    }
+
+    public String getFooterValue(Customer aCustomer) {
+        String result = "<P>You owe <EM>" +
+            String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
+        result += "On this rental you earned <EM>" +
+            String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+            "</EM> frequent renter points<P>";
+        return result;
+    }
+
+    public String getHeadValue(Customer aCustomer) {
+        return "<H1>Rentals for <EM>" + aCustomer.getName() +
+            "</EM></H1><P>\n";
+    }
+
+
+
+    public static void main(String[] args) {}
 }
